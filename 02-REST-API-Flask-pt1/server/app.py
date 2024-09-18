e0eee90fde9304a6b8095e1730eeb7e6d2d00a15
betalantz <betalantz@gmail.com>
2024-09-18 10:21:48 -0700

    completes code and reorg comments

--- 02-REST-API-Flask-pt1/server/app.py
+++ 02-REST-API-Flask-pt1/server/app.py
@@ -1,40 +1,41 @@
 #!/usr/bin/env python3
 
 # 📚 Review With Students:
-    # API Fundamentals
-    # MVC Architecture and Patterns / Best Practices
-    # RESTful Routing
-    # | HTTP Verb 	|       Path       	| Description        	|
-    # |-----------	|:----------------:	|--------------------	|
-    # | GET       	|   /productions   	| READ all resources 	|
-    # | GET       	| /productions/:id 	| READ one resource   	|
-    # | POST      	|   /productions   	| CREATE one resource 	|
-    # | PATCH/PUT 	| /productions/:id 	| UPDATE one resource	|
-    # | DELETE    	| /productions/:id 	| DESTROY one resource 	|
+# API Fundamentals
+# MVC Architecture and Patterns / Best Practices
+# RESTful Routing
+# | HTTP Verb 	|       Path       	| Description        	|
+# |-----------	|:----------------:	|--------------------	|
+# | GET       	|   /productions   	| READ all resources 	|
+# | GET       	| /productions/:id 	| READ one resource   	|
+# | POST      	|   /productions   	| CREATE one resource 	|
+# | PATCH/PUT 	| /productions/:id 	| UPDATE one resource	|
+# | DELETE    	| /productions/:id 	| DESTROY one resource 	|
 
-    # Serialization
-    # Postman
+# Serialization
+# Postman
 
 # Set Up:
-    # In Terminal, `cd` into `server` and run the following:
-        # export FLASK_APP=app.py
-        # export FLASK_RUN_PORT=5000
-        # flask db init
-        # flask db revision --autogenerate -m 'Create tables' 
-        # flask db upgrade 
-        # python seed.py
+# In Terminal, `cd` into `server` and run the following:
+# export FLASK_APP=app.py
+# export FLASK_RUN_PORT=5555
+# flask db init
+# flask db revision --autogenerate -m 'Create tables'
+# flask db upgrade
+# python seed.py
 
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, jsonify, make_response, request
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from models import CastMember, Production, db
 
 # 1. ✅ Import `Api` and `Resource` from `flask_restful`
-    # ❓ What do these two classes do at a higher level? 
+# ❓ What do these two classes do at a higher level?
 
-from models import db, Production, CrewMember
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # Note: `app.json.compact = False` configures JSON responses to print on indented lines
 app.json.compact = False
 
@@ -42,57 +43,122 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 # 2. ✅ Initialize the Api
-    # `api = Api(app)`
+# `api = Api(app)`
+api = Api(app)
 
 # 3. ✅ Create a Production class that inherits from Resource
 
-# 4. ✅ Create a GET (All) Route
+
+class Productions(Resource):
+
+    # 4. ✅ Create a GET (All) Route
     # 4.1 Make a `get` method that takes `self` as a param.
-    # 4.2 Create a `productions` array.
-    # 4.3 Make a query for all productions. For each `production`, create a dictionary 
-    # containing all attributes before appending to the `productions` array.
-    # 4.4 Create a `response` variable and set it to: 
-    #  #make_response(
-    #       jsonify(productions),
-    #       200
-    #  )
-    # 4.5 Return `response`.
-    # 4.6 After building the route, run the server and test in the browser.
-  
-# 5. ✅ Serialization
-    # This is great, but there's a cleaner way to do this! Serialization will allow us to easily add our 
-    # associations as well.
-    # Navigate to `models.py` for Steps 6 - 9.
+    def get(self):
+        # 4.2 Create a `productions` list.
+        productions = Production.query.all()
+        # 4.3 Make a query for all productions. For each `production`, create a dictionary
+        # containing all attributes before appending to the `productions` list.
+        # prods_list = [
+        #     {
+        #         "title": production.title,
+        #         "genre": production.genre,
+        #         "budget": production.budget,
+        #         "image": production.image,
+        #         "director": production.director,
+        #         "ongoing": production.ongoing,
+        #     }
+        #     for production in productions
+        # ]
+        # 10. ✅ Use our serializer to format our response to be cleaner
+        # 10.1 Query all of the productions, convert them to a dictionary with `to_dict` before setting them to a list.
+        prods_list = [
+            production.to_dict(only=("title", "director")) for production in productions
+        ]
+        # 4.4 Create a `response` variable and set it to:
+        #  #make_response(
+        #       jsonify(productions),
+        #       200
+        #  )
+        # 10.2 Invoke `make_response`, pass it the production list along with a status of 200. Set `make_response` to a
+        # `response` variable.
+        # 10.3 Return the `response` variable.
+        # 10.4 After building the route, run the server and test your results in the browser.
+        response = make_response(
+            prods_list, 200
+        )  # make_response uses jsonify to serialize dictionaries
+        # 4.5 Return `response`.
+        return response
+        # 4.6 After building the route, run the server and test in the browser.
 
-# 10. ✅ Use our serializer to format our response to be cleaner
-    # 10.1 Query all of the productions, convert them to a dictionary with `to_dict` before setting them to a list.
-    # 10.2 Invoke `make_response`, pass it the production list along with a status of 200. Set `make_response` to a 
-    # `response` variable.
-    # 10.3 Return the `response` variable.
-    # 10.4 After building the route, run the server and test your results in the browser.
- 
-# 11. ✅ Create a POST Route
-    # Prepare a POST request in Postman. Under the `Body` tab, select `form-data` and fill out the body 
-    # of a production request. 
-    
-    # Create the POST route 
+        # 5. ✅ Serialization
+        # This is great, but there's a cleaner way to do this! Serialization will allow us to easily add our
+        # associations as well.
+        # Navigate to `models.py` for Steps 6 - 9.
+
+    # 11. ✅ Create a POST Route
+    # Prepare a POST request in Postman. Under the `Body` tab, select `form-data` and fill out the body
+    # of a production request.
+
+    # Create the POST route
     # 📚 Review With Students: request object
-    
-    # 11.1 Create a `post` method and pass it `self`.
-    # 11.2 Create a new production from the `request.form` object.
-    # 11.3 Add and commit the new production.
-    # 11.4 Convert the new production to a dictionary with `to_dict`
-    # 11.5 Set `make_response` to a `response` variable and pass it the new production along with a status of 201.
-    # 11.6 Test the route in Postman.
 
-   
+    # 11.1 Create a `post` method and pass it `self`.
+    def post(self):
+        request_json = request.get_json()
+        # 11.2 Create a new production from the `request.json` object.
+        new_production = Production(
+            title=request_json["title"],
+            genre=request_json["genre"],
+            budget=request_json["budget"],
+            image=request_json["image"],
+            director=request_json["director"],
+            description=request_json["description"],
+            ongoing=request_json["ongoing"],
+        )
+        # 11.3 Add and commit the new production.
+        db.session.add(new_production)
+        db.session.commit()
+
+        # 11.4 Convert the new production to a dictionary with `to_dict`
+        # 11.5 Set `make_response` to a `response` variable and pass it the new production along with a status of 201.
+        return make_response(new_production.to_dict(), 201)
+
+        # 11.6 Test the route in Postman.
+
+
+"""
+@app.route('/productions', methods=['GET', 'POST'])
+def index():
+    if request.method == "GET":
+        ...do the query.all logic
+    elif request.method == "POST":
+        ...do the post logic
+
+"""
+
+
 # 12. ✅ Add the new route to our api with `api.add_resource`
+api.add_resource(Productions, "/productions")
+
 
 # 13. ✅ Create a GET (One) route
-    # 13.1 Build a class called `ProductionByID` that inherits from `Resource`.
-    # 13.2 Create a `get` method and pass it the id along with `self`. (This is how we will gain access to 
+# 13.1 Build a class called `ProductionByID` that inherits from `Resource`.
+class ProductionByID(Resource):
+
+    # 13.2 Create a `get` method and pass it the id along with `self`. (This is how we will gain access to
     # the id from our request)
-    # 13.3 Make a query for our production by the `id` and build a `response` to send to the browser.
+    def get(self, id):
+        # 13.3 Make a query for our production by the `id` and build a `response` to send to the browser.
+        production = db.session.get(Production, id)
+        # production = Production.query.get(id)
+        if not production:
+            return make_response({"error": "Requested production not found"}, 404)
+        return make_response(production.to_dict(), 200)
 
 
 # 14. ✅ Add the new route to our api with `api.add_resource`
+api.add_resource(ProductionByID, "/productions/<int:id>")
+
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
--- 02-REST-API-Flask-pt1/server/models.py
+++ 02-REST-API-Flask-pt1/server/models.py
@@ -1,12 +1,14 @@
-
 from flask_sqlalchemy import SQLAlchemy
+
 # 6. ✅ Import `SerializerMixin` from `sqlalchemy_serializer`
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
+
 # 7. ✅ Pass `SerializerMixin` to `Productions`
-class Production(db.Model):
-    __tablename__ = 'productions'
+class Production(db.Model, SerializerMixin):
+    __tablename__ = "productions"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
@@ -19,18 +21,20 @@ class Production(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    crew_members = db.relationship('CrewMember', backref='production')
+    cast_members = db.relationship("CastMember", backref="production")
 
     # 7.1 ✅ Create a serialize rule that will help add our `crew_members` to the response and remove created_at and updated_at.
-        #7.2 Demo serialize_only by only allowing title to be included in the response
-        #    once done remove or comment the serialize_only line. 
+    # 7.2 Demo serialize_only by only allowing title to be included in the response
+    #    once done remove or comment the serialize_only line.
+    serialize_rules = ("-created_at", "-update_at", "-cast_members.production")
 
     def __repr__(self):
-        return f'<Production Title:{self.title}, Genre:{self.genre}, Budget:{self.budget}, Image:{self.image}, Director:{self.director},ongoing:{self.ongoing}>'
+        return f"<Production Title:{self.title}, Genre:{self.genre}, Budget:{self.budget}, Image:{self.image}, Director:{self.director},ongoing:{self.ongoing}>"
 
-# 8. ✅ Pass `SerializerMixin` to `CrewMember`
-class CrewMember(db.Model):
-    __tablename__ = 'crew_members'
+
+# 8. ✅ Pass `SerializerMixin` to `CastMember`
+class CastMember(db.Model, SerializerMixin):
+    __tablename__ = "cast_members"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -38,11 +42,12 @@ class CrewMember(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    production_id = db.Column(db.Integer, db.ForeignKey('productions.id'))
-    
-    # 8.1 ✅ Create a serialize rule that will help add our `production` to the response.
-      
-    def __repr__(self):
-        return f'<Production Name:{self.name}, Role:{self.role}'
+    production_id = db.Column(db.Integer, db.ForeignKey("productions.id"))
 
- # 9. ✅ Navigate back to `app.py` for further steps.
+    # 8.1 ✅ Create a serialize rule that will help add our `production` to the response.
+
+    def __repr__(self):
+        return f"<CastMember Name:{self.name}, Role:{self.role}"
+
+
+# 9. ✅ Navigate back to `app.py` for further steps.
