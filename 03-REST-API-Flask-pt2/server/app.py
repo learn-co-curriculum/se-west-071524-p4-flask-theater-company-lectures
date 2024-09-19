bcf3065cdc92b4818df0b0d59d3a46f25ce7346e
betalantz <betalantz@gmail.com>
2024-09-19 09:29:45 -0700

    completes 03 with comments

--- 03-REST-API-Flask-pt2/server/app.py
+++ 03-REST-API-Flask-pt2/server/app.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python3
 # 📚 Review With Students:
-    # REST
-    # Status codes
-    # Error handling 
+# REST
+# Status codes
+# Error handling
 # Set up:
-    # cd into server and run the following in the terminal
-    # export FLASK_APP=app.py
-    # export FLASK_RUN_PORT=5000
-    # flask db init
-    # flask db revision --autogenerate -m'Create tables' 
-    # flask db upgrade 
-    # python seed.py
-from flask import Flask, request, make_response, abort
+# cd into server and run the following in the terminal
+# export FLASK_APP=app.py
+# export FLASK_RUN_PORT=5000
+# flask db init
+# flask db revision --autogenerate -m'Create tables'
+# flask db upgrade
+# python seed.py
+from flask import Flask, abort, make_response, request
 from flask_migrate import Migrate
-
 from flask_restful import Api, Resource
+from models import CastMember, Production, db
 
 # 1.✅ Import NotFound from werkzeug.exceptions for error handling
-
-
-from models import db, Production, CastMember
+from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 db.init_app(app)
 
-
+Api.error_router = lambda self, handler, e: handler(e)
 api = Api(app)
+
 
 class Productions(Resource):
     def get(self):
@@ -44,13 +43,13 @@ class Productions(Resource):
 
     def post(self):
         new_production = Production(
-            title=request.form['title'],
-            genre=request.form['genre'],
-            budget=int(request.form['budget']),
-            image=request.form['image'],
-            director=request.form['director'],
-            description=request.form['description'],
-            ongoing=bool(request.form['ongoing']),
+            title=request.get_json()["title"],
+            genre=request.get_json()["genre"],
+            budget=int(request.get_json()["budget"]),
+            image=request.get_json()["image"],
+            director=request.get_json()["director"],
+            description=request.get_json()["description"],
+            ongoing=bool(request.get_json()["ongoing"]),
         )
 
         db.session.add(new_production)
@@ -63,48 +62,93 @@ class Productions(Resource):
             201,
         )
         return response
-api.add_resource(Productions, '/productions')
+
+
+api.add_resource(Productions, "/productions")
 
 
 class ProductionByID(Resource):
-    def get(self,id):
+    def get(self, id):
         production = Production.query.filter_by(id=id).first()
-# 3.✅ If a production is not found raise the NotFound exception
-    # 3.1 AND/OR use abort() to create a 404 with a customized error message
-    
-        production_dict = production.to_dict()
-        response = make_response(
-            production_dict,
-            200
-        )
-        
+        # 3.✅ If a production is not found raise the NotFound exception
+        # 3.1 AND/OR use abort() to create a 404 with a customized error message
+        if not production:
+            # return make_response({"message": "Production not fouund"}, 404)
+            raise NotFound
+        production_dict = production.to_dict(rules=("actors",))
+        response = make_response(production_dict, 200)
+
         return response
 
-# 4.✅ Patch
+    # 4.✅ Patch
     # 4.1 Create a patch method that takes self and id
-    # 4.2 Query the Production from the id
-    # 4.3 If the production is not found raise the NotFound exception AND/OR use abort() to create a 404 with a customized error message
-    # 4.4 Loop through the request.form object and update the productions attributes. Note: Be cautions of the data types to avoid errors.
-    # 4.5 add and commit the updated production 
-    # 4.6 Create and return the response
-  
-# 5.✅ Delete
-    # 5.1 Create a delete method, pass it self and the id
-    # 5.2 Query the Production 
-    # 5.3 If the production is not found raise the NotFound exception AND/OR use abort() to create a 404 with a customized error message
-    # 5.4 delete the production and commit 
-    # 5.5 create a response with the status of 204 and return the response 
-  
+    def patch(self, id):
+        # 4.2 Query the Production from the id
+        req_json = request.get_json()
+        production = Production.query.filter_by(id=id).first()
 
-   
-api.add_resource(ProductionByID, '/productions/<int:id>')
+        # 4.3 If the production is not found raise the NotFound exception AND/OR use abort() to create a 404 with a customized error message
+        if not production:
+            # abort(404)
+            raise NotFound
+        # 4.4 Loop through the request.get_json() dict and update the productions attributes. Note: Be cautions of the data types to avoid errors.
+        for key, value in req_json.items():
+            setattr(production, key, value)
+        # 4.5 add and commit the updated production
+        db.session.add(production)
+        db.session.commit()
+        # 4.6 Create and return the response
+        return make_response(production.to_dict(), 202)
+
+    """
+    fetch('/productions, {
+            method: "POST",
+            headers: {...},
+            body: JSON.stringify(new_production)
+        
+        })
+    
+    """
+
+    # 5.✅ Delete
+    # 5.1 Create a delete method, pass it self and the id
+    def delete(self, id):
+        # 5.2 Query the Production
+        production = Production.query.filter_by(id=id).first()
+        # 5.3 If the production is not found raise the NotFound exception AND/OR use abort() to create a 404 with a customized error message
+        if not production:
+            raise NotFound
+
+        # 5.4 delete the production and commit
+        db.session.delete(production)
+        db.session.commit()
+        # 5.5 create a response with the status of 204 and return the response
+        return make_response("", 204)
+
+
+api.add_resource(ProductionByID, "/productions/<int:id>")
+
 
 # 2.✅ use the @app.errorhandler() decorator to handle Not Found
-    # 2.1 Create the decorator and pass it NotFound
-    # 2.2 Use make_response to create a response with a message and the status 404
-    # 2.3 return t he response
+# 2.1 Create the decorator and pass it NotFound
+# 2.2 Use make_response to create a response with a message and the status 404
+# 2.3 return t he response
+@app.errorhandler(NotFound)
+def handle_not_found(exception):
+    # import ipdb
+
+    # ipdb.set_trace()
+    return make_response(
+        {
+            "message": f"Not found: Sorry the {request.path.strip('/1234567890s').title()} you are looking for doesn't exist"
+        },
+        404,
+    )
+
+
+app.register_error_handler(404, handle_not_found)
 
 
 # To run the file as a script
-# if __name__ == '__main__':
-#     app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
--- 03-REST-API-Flask-pt2/server/models.py
+++ 03-REST-API-Flask-pt2/server/models.py
@@ -1,10 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+
+db = SQLAlchemy(metadata=metadata)
+
 
 class Production(db.Model, SerializerMixin):
-    __tablename__ = 'productions'
+    __tablename__ = "productions"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
@@ -16,26 +25,59 @@ class Production(db.Model, SerializerMixin):
     ongoing = db.Column(db.Boolean)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-    cast_members = db.relationship('CastMember', backref='production')
-        
-    serialize_rules = ('-cast_members.production','-created_at','-updated_at')
 
+    cast_members = db.relationship("CastMember", back_populates="production")
+    actors = association_proxy(
+        "cast_members", "actor", creator=lambda actor_obj: CastMember(actor=actor_obj)
+    )
+
+    serialize_rules = (
+        "-cast_members.production",  # this rule prevents recursions
+        "-cast_members.production_id",
+        "-created_at",
+        "-updated_at",
+    )
 
     def __repr__(self):
-        return f'<Production Title:{self.title}, Genre:{self.genre}, Budget:{self.budget}, Image:{self.image}, Director:{self.director},ongoing:{self.ongoing}>'
+        return f"<Production Title:{self.title}, Genre:{self.genre}, Budget:{self.budget}, Image:{self.image}, Director:{self.director},ongoing:{self.ongoing}>"
 
-class CastMember(db.Model, SerializerMixin):
-    __tablename__ = 'cast_members'
+
+class Actor(db.Model, SerializerMixin):
+    __tablename__ = "actors"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    cast_members = db.relationship("CastMember", back_populates="actor")
+
+    serialize_rules = ("-cast_members.actor",)  # this rule prevents recursions
+
+    def __repr__(self):
+        return f"<Actor id: {self.id} name: {self.name} email: {self.email} >"
+
+
+class CastMember(db.Model, SerializerMixin):
+    __tablename__ = "cast_members"
+
+    id = db.Column(db.Integer, primary_key=True)
     role = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-    production_id = db.Column(db.Integer, db.ForeignKey('productions.id'))
-    
-    serialize_rules = ('-production.cast_members',)
+    production_id = db.Column(db.Integer, db.ForeignKey("productions.id"))
+    actor_id = db.Column(db.Integer, db.ForeignKey("actors.id"))
+
+    production = db.relationship("Production", back_populates="cast_members")
+    actor = db.relationship("Actor", back_populates="cast_members")
+
+    serialize_rules = (
+        "-production.cast_members",  # this rule prevents recursions
+        "-actor.cast_members",  # this rule prevents recursions
+        "-created_at",
+        "-updated_at",
+    )
 
     def __repr__(self):
-        return f'<Production Name:{self.name}, Role:{self.role}'
-
+        return f"<Production Name:{self.name}, Role:{self.role}"
