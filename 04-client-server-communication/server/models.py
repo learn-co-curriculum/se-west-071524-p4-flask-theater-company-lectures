954f3daf403c38fb9a8dc4d8e690996c0ed10312
betalantz <betalantz@gmail.com>
2024-09-19 09:44:04 -0700

    updates 04 with cast_members

--- 04-client-server-communication/server/models.py
+++ 04-client-server-communication/server/models.py
@@ -1,5 +1,5 @@
 # 📚 Review With Students:
-    # Validations and Invalid Data
+# Validations and Invalid Data
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
@@ -8,12 +8,13 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
+
 class Production(db.Model, SerializerMixin):
-    __tablename__ = 'productions'
+    __tablename__ = "productions"
 
     id = db.Column(db.Integer, primary_key=True)
 
-# 2.✅ Add Constraints to the Columns        
+    # 2.✅ Add Constraints to the Columns
 
     title = db.Column(db.String)
     genre = db.Column(db.String)
@@ -24,34 +25,33 @@ class Production(db.Model, SerializerMixin):
     ongoing = db.Column(db.Boolean, default=True)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-    crew_members = db.relationship('CrewMember', backref='production')
-        
-    serialize_rules = ('-crew_members.production',)
+    cast_members = db.relationship("CastMember", backref="production")
 
+    serialize_rules = ("-cast_members.production",)
 
-# 3.✅ Use the "validates" decorator to create a validation for images
+    # 3.✅ Use the "validates" decorator to create a validation for images
     # 3.1 Pass the decorator 'image'
     # 3.2 Define a validate_image method, pass it self, key and image_path
-    # 3.3 If .jpg is not in the image passed, raise the ValueError exceptions else 
+    # 3.3 If .jpg is not in the image passed, raise the ValueError exceptions else
     # return the image_path
-    # Note: Feel free to try out more validations!  
-  
-# 4.✅ navigate to app.py
-    def __repr__(self):
-        return f'<Production Title:{self.title}, Genre:{self.genre}, Budget:{self.budget}, Image:{self.image}, Director:{self.director},ongoing:{self.ongoing}>'
+    # Note: Feel free to try out more validations!
 
-class CrewMember(db.Model, SerializerMixin):
-    __tablename__ = 'crew_members'
+    # 4.✅ navigate to app.py
+    def __repr__(self):
+        return f"<Production Title:{self.title}, Genre:{self.genre}, Budget:{self.budget}, Image:{self.image}, Director:{self.director},ongoing:{self.ongoing}>"
+
+
+class CastMember(db.Model, SerializerMixin):
+    __tablename__ = "cast_members"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     role = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-    production_id = db.Column(db.Integer, db.ForeignKey('productions.id'))
-    
-    serialize_rules = ('-production.crew_members',)
+    production_id = db.Column(db.Integer, db.ForeignKey("productions.id"))
+
+    serialize_rules = ("-production.cast_members",)
 
     def __repr__(self):
-        return f'<Production Name:{self.name}, Role:{self.role}'
-
+        return f"<Production Name:{self.name}, Role:{self.role}"
